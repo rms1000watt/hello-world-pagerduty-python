dc17b912b6700ac63498e431e9c0987425f9f633
Ryan Smith <rms1000watt@gmail.com>
2019-01-29 12:10:27 -0800

    working

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import os
 import json
+import time
 import datetime
 import requests
 
-
-def main():
-  api_key = get_api_key()
-  dedup_key = get_dedup_key("31")
-  print(dedup_key)
-
-  # create_event(api_key, "test-service-31", dedup_key)
-
+# https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
 
 def get_api_key():
   return os.environ["PD_API_KEY"]
@@ -20,10 +14,21 @@ def get_dedup_key(service_str):
   return "dedup-key-%s-%s" %(datetime.datetime.now().isocalendar()[1], service_str)
 
 
-def create_event(api_key, service_name, dedup_key):
-  header = {"Content-Type": "application/json"}
+def resolve_event(api_key, service_name, dedup_key):
+  print("Resolving Event: service_name=%s dedup_key=%s" %(service_name, dedup_key))
 
-  # https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
+  payload = {
+    "routing_key": api_key,
+    "event_action": "resolve",
+    "dedup_key": dedup_key
+  }
+
+  make_request(payload)
+
+
+def create_event(api_key, service_name, dedup_key):
+  print("Creating Event: service_name=%s dedup_key=%s" %(service_name, dedup_key))
+
   payload = {
     "routing_key": api_key,
     "event_action": "trigger",
@@ -31,20 +36,41 @@ def create_event(api_key, service_name, dedup_key):
     "payload": {
       "summary": "Ryan smith test please disregard",
       "source": "ryan-smith-test-please-disregard",
-      "severity": "info",
+      "severity": "warning",
       "component": service_name,
       "class":  "Deployment Notification"
     }
   }
 
-  response = requests.post('https://events.pagerduty.com/v2/enqueue',
-    data=json.dumps(payload),
-    headers=header)
+  make_request(payload)
 
-  if response.json()["status"] == "success":
-    print('Incident created with with dedup key (also known as incident / alert key) of ' + '"' + response.json()['dedup_key'] + '"')
-  else:
-    print(response.text) # print error message if not successful
+
+def make_request(payload):
+  headers = {"Content-Type": "application/json"}
+  try:
+    response = requests.post('https://events.pagerduty.com/v2/enqueue',
+      data=json.dumps(payload),
+      headers=headers)
+
+    if response.json()["status"] == "success":
+      print("Success")
+    else:
+      print("Failed: status=%s response=%s" %(response.json()["status"], response.text))
+  except Exception as e:
+    print("Failed: %s" %(e))
+
+
+def main():
+  api_key = get_api_key()
+  dedup_key = get_dedup_key("31")
+
+  create_event(api_key, "test-service-31", dedup_key)
+
+  print("Sleeping 60s")
+  time.sleep(60)
+
+  resolve_event(api_key, "test-service-31", dedup_key)
+
 
 if __name__ == "__main__":
   main()
